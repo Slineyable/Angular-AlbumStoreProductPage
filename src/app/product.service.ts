06d0b2277af0fd090e4d716bb2d70a0dbd47879d
John Sliney <john.sliney@lcs.com>
2022-04-26 13:38:58 -0400

    finish part 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Album } from './album';
 import 'rxjs/add/operator/map'
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
